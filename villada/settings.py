68edb1af0f29315f36b7eaca81f3862b15f7ae52
Agus Salonia <agusalonia04@gmail.com>
2022-10-28 08:59:16 -0300

    Toma Finux pullea

--- villada/settings.py
+++ villada/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'villadApp',
 ]
 
 MIDDLEWARE = [
@@ -51,10 +52,12 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'villada.urls'
 
+import os
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- villada/urls.py
+++ villada/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from villadApp.views import ITS
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("villada/", ITS)
 ]
