876623d104017ec379d3a1ff4eac28716671f7b3
MarcoFm1 <marco.fini22@gmail.com>
2022-11-18 13:59:26 -0300

    act_login

--- villadApp/static/js/login.js
+++ villadApp/static/js/login.js
@@ -1,15 +1,38 @@
 var register = document.getElementById('div_sec');
 var login = document.getElementById('div_prin');
-var botonLog = document.getElementById('btn_dha');
-var botonReg = document.getElementById('btn_reg')
-var dha = document.getElementById('btn_dha')
 
 function register() {
-    
+    if (register.classList.contains('animacionDivReg')){
+        register.classList.remove('animacionDivReg');
+        register.classList.add('animacionDivRegVuelta');
+        register.style.opacity = 1;
+        register.style.left = '36%';
+
+
+    }
+    else{
+        register.classList.remove('animacionDivRegVuelta');
+        register.classList.add('animacionDivReg');
+        register.style.opacity = 0;
+        register.style.left = '20%';
+    }
 }
 
 function login() {
+    if (login.classList.contains('animacionDivLog')){
+        login.classList.remove('animacionDivLog');
+        login.classList.add('animacionDivLogVuelta');
+        login.style.opacity = 1;
+        login.style.left = '36%';
 
+
+    }
+    else{
+        login.classList.remove('animacionDivLogVuelta');
+        login.classList.add('animacionDivLog');
+        login.style.opacity = 0;
+        login.style.left = '20%';
+    }
 }
 
 
