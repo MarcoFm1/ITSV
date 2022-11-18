bf4d292049e5ea5058aa1fdae032eadc60ed70be
alejito <diazbroiloalejol@gmail.com>
2022-11-18 11:45:40 -0300

    dejame pusheaaaaar

--- villadApp/views.py
+++ villadApp/views.py
@@ -8,3 +8,19 @@ def ITS(request):
 
 def LOGIN(request):
     return render(request,'../templates/villadApp/login.html')
+
+def PROFILE(request,tipo,nombre):
+    
+    if tipo == 'estudiante':
+        nombre_final = ''
+        for i in nombre:
+            if i.isupper():
+                nombre_final += f' {i}'
+            else:
+                nombre_final += f'{i}'
+    else:
+        print('SE RASCA LOS HUEVOS')
+    
+    nombre = nombre_final
+    response = {'nombre':nombre}
+    return render(request,'../templates/villadApp/profile.html',response)
