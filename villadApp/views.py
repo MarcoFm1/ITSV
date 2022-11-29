587cbcb3bbd52d7ff7c28e76b278cd64e10810e3
MarcoFm1 <marco.fini22@gmail.com>
2022-11-29 19:34:29 -0300

    ERA TAN SIMPLE AAAA

--- villadApp/views.py
+++ villadApp/views.py
@@ -1,20 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
-from django.contrib import messages
-from .forms import UserRegistrationForm
+from django.contrib.auth.forms import UserCreationForm
 
-def register(request):
-    if request.method == 'POST':
-        form = UserRegistrationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            messages.success(request, f'Fue creada, te podes loguear')    
-            return redirect('../templates/villadApp/login.html')
-    else:
-        form = UserRegistrationForm()
-
-    context = {'form': form}
-    return render(request, '../templates/villadApp/register.html', context)
 
 # Create your views here.
 
@@ -22,11 +9,19 @@ def ITS(request):
     return render(request,'../templates/villadApp/main.html')
 
 def REGISTER(request):
-    return render(request,'../templates/villadApp/register.html')
+    form = UserCreationForm()
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+    context = {'form': form}
+    return render(request, '../templates/villadApp/register.html', context)
 
 def LOGIN(request):
-    return render(request,'../templates/villadApp/login.html')
+    context = {}
+    return render(request,'../templates/villadApp/login.html', context)
 
-def QUERY(request):
-    return render(request,'../templates/villadApp/query.html')
-
+def PRUEBA():
+    print('enie')
