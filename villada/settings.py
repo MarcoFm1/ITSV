271d6b1c507d3a5de10dee6499622ac559442a4c
JoaquinMorais <joaquinmorais2005@gmail.com>
2022-11-30 13:44:46 -0300

    modificado usuario bdi por villada

--- villada/settings.py
+++ villada/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', 
         'NAME': 'VilladaApp',
-        'USER': 'bdi',
+        'USER': 'villada',
         'PASSWORD': 'pepe1234',
         'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
         'PORT': '3306',
