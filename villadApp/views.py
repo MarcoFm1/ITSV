bb3d8101ffc5b95e7841b63180cedf132d2b408f
MarcoFm1 <marco.fini22@gmail.com>
2022-12-01 00:31:06 -0300

    ahora con privacidad

--- villadApp/views.py
+++ villadApp/views.py
@@ -11,7 +11,7 @@ def ITS(request):
     return render(request,'../templates/villadApp/index.html')
 
 def REGISTER(request):
-    if request.user.is_authenticed:
+    if request.user.is_authenticated:
         return redirect('home')
     else:
         form = CreateUserForm()
@@ -36,7 +36,6 @@ def PROFILE(request,tipo,nombre):
     if tipo == 'estudiante':
         estudiante = Alumno.objects.all().get(nombre = nombre)
         materia_horario = MateriaHorario.objects.all().filter(dia__cronograma__curso = estudiante.curso)
-
         modulos_materias = {}
         for i in materia_horario:
             if i.modulo.orden in modulos_materias:
@@ -61,7 +60,6 @@ def DESCRIPCION(request,objeto,elemento,atributo):
         modulo = Modulo.objects.all().get(orden = int(elemento))
         curso = Curso.objects.all().get(anio__anio = int(atributo[0]),division__division = atributo[1])
         materia_horario = MateriaHorario.objects.all().filter(dia__cronograma__curso = curso,modulo__orden = int(elemento)).order_by('dia__dia')
-
         modulos_materias = {}
         for i in materia_horario:
             modulos_materias[f'{i.dia.dia}'] = {'nombre':i.materia.nombre,'abreviacion':i.materia.abreviado} 
@@ -81,32 +79,33 @@ def DESCRIPCION(request,objeto,elemento,atributo):
                     modulos_materias[i.modulo.orden][f'{i.dia.dia}'] = {'nombre':i.materia.nombre,'abreviacion':i.materia.abreviado}
                 else:
                     modulos_materias[i.modulo.orden] = {'orden':i.modulo,f'{i.dia.dia}':{'nombre':i.materia.nombre,'abreviacion':i.materia.abreviado}}
-
             response = {'objeto':objeto,'elemento':elemento,'atributo':atributo,'modulos_materia':modulos_materias}
             
         else:
             materia_horario = MateriaHorario.objects.all().filter(dia__cronograma__curso = curso, dia__dia__dia = elemento).order_by('modulo__orden')
             response = {'objeto':objeto,'elemento':elemento,'atributo':atributo,'materia_horario':materia_horario}
-
         return render(request,'../templates/villadApp/materias.html',response)
     else:
         return redirect('villada')
 
 def LOGIN(request):
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        if request.method == 'POST':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
 
-        user = authenticate(request, username = username, password = password)
+            user = authenticate(request, username = username, password = password)
 
-        if user is not None:
-            login(request, user)
-            return redirect('home')
-        else:
-            messages.info(request, 'Nombre o Token incorrectos')
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+            else:
+                messages.info(request, 'Nombre o Token incorrectos')
 
-    context = {}
-    return render(request,'../templates/villadApp/login.html', context)
+        context = {}
+        return render(request,'../templates/villadApp/login.html', context)
 
 
 def LOGOUT(request):
@@ -115,4 +114,4 @@ def LOGOUT(request):
 
 @login_required(login_url='login')
 def CURSOS(request):
-    return render(request,'../templates/villadApp/cursos.html')
+    return render(request,'../templates/villadApp/cursos.html')    
