7c90f9670d45820b7c7176c118cd8a8ae6db2317
Agus Salonia <agusalonia04@gmail.com>
2022-12-01 12:08:22 -0300

    profile godin godon

---
+++ villadApp/static/js/profile.js
@@ -0,0 +1,14 @@
+function horarios(){
+    document.getElementById('principal-menu').style.display = 'flex';
+    document.getElementById('registro-faltas').style.display = 'none';
+}
+
+function faltas(){
+    document.getElementById('principal-menu').style.display = 'none';
+    document.getElementById('registro-faltas').style.display = 'flex';
+}
+
+
+
+
+
